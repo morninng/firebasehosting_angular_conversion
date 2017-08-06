f14802fa06f47c9638bb5949551a15c9d9870d51
morninng <mr_morninng_star_dash@yahoo.co.jp>
2017-08-06 20:31:02 +0900

    build and move succeed

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 const gulp = require('gulp');
 const fs = require('fs');
 const runSequence = require('run-sequence');
+const rimraf = require('rimraf');
+var del = require('del')
+const shell = require('gulp-shell')
  
 gulp.task('hello', function() {
   console.log('Hello gulp!');
 });
  
 
+gulp.task('build_angular', () => {
+    console.log("<<<<<build_angular>>>>>>>");
+   return gulp.src('./src/index.html', {read: false})
+  .pipe(shell([
+    'echo _____shell_script_start_____',
+    'ng build --prod --aot=true',
+    'echo ______shell_script_finish____',
+  ]))
+})
+
+
 gulp.task('convert_indexhtml', function() {
-  console.log('convert_indexhtml start');
+  console.log('<<<<<convert_indexhtml start>>>>>>>');
 
      gulp.src( './dist/index.html')
     .pipe(fs.readFile("./dist/index.html", "utf-8", (err, _data) => {
@@ -35,22 +49,30 @@ gulp.task('convert_indexhtml', function() {
 
 
 gulp.task('transfer_convertedfile', ()=>{
-    console.log("transfer_convertedfile");
-    gulp.src([
+    console.log("<<<<<transfer_convertedfile>>>>>>>");
+    return gulp.src([
         './dist/index.ect'
     ])
     .pipe(gulp.dest('./../../functions/views'))
 
 })
 
-
+/*
 gulp.task('delete_index_html', ()=>{
-    console.log("delete_index_html");
+    console.log("<<<<<delete_index_html>>>>>>>");
+     del(['./dist/index.html', './dist/index.ect'], function(){ 
+         console.log("file deleted")
+    });
 
 })
+*/
 
 gulp.task('transfer_dist_to_public', ()=>{
-    console.log("transfer_dist_to_public");
+    console.log("<<<<<transfer_dist_to_public>>>>>>>");
+    gulp.src([
+        './dist/*', '!./dist/index.html', '!./dist/index.ect'
+    ])
+    .pipe(gulp.dest('./../../public'))
 
 })
 
@@ -59,9 +81,9 @@ gulp.task('transfer_dist_to_public', ()=>{
 // http://blog.anatoo.jp/entry/20140420/1397995711 
 
 gulp.task('build', function() {
-  runSequence('convert_indexhtml', 'transfer_convertedfile', 'delete_index_html', 'transfer_dist_to_public');
+  runSequence('build_angular', 'convert_indexhtml');
 });
 
-gulp.task('sequence_test', function() {
-  runSequence('transfer_convertedfile', 'delete_index_html', 'transfer_dist_to_public');
+gulp.task('move', function() {
+  runSequence( 'transfer_convertedfile',  'transfer_dist_to_public');
 });
